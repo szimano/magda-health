38e684b3fbe2b1f74a98ca4a6e4b565f851cc4a9
szimano <szimano@szimano.org>
2015-05-25 21:54:35 +0200

    precompiles templates

---
+++ assets/js/templates.js
@@ -0,0 +1,90 @@
+(function() {(window.nunjucksPrecompiled = window.nunjucksPrecompiled || {})["coaching.html"] = (function() {function root(env, context, frame, runtime, cb) {
+var lineno = null;
+var colno = null;
+var output = "";
+try {
+output += "<!DOCTYPE HTML>\r\n<!--\r\n\tSpectral by HTML5 UP\r\n\thtml5up.net | @n33co\r\n\tFree for personal and commercial use under the CCA 3.0 license (html5up.net/license)\r\n-->\r\n<html id='site'>\r\n\t<body>\r\n\t\t<script src=\"assets/js/nunjucks.min.js\"></script>\t\t\r\n\t\t<script>\r\n\t\tvar env = new nunjucks.configure('/magda-health', {});\r\n\t\tvar res = nunjucks.render('coaching-src.html');\r\n\t\tvar newDoc = document.open(\"text/html\", \"replace\");\r\n\t\tnewDoc.write(res);\r\n\t\tnewDoc.close();\r\n\t\t</script>\r\n\t</body>\r\n</html>";
+cb(null, output);
+;
+} catch (e) {
+  cb(runtime.handleError(e, lineno, colno));
+}
+}
+return {
+root: root
+};
+})();
+})();
+
+(function() {(window.nunjucksPrecompiled = window.nunjucksPrecompiled || {})["contact.html"] = (function() {function root(env, context, frame, runtime, cb) {
+var lineno = null;
+var colno = null;
+var output = "";
+try {
+output += "<!DOCTYPE HTML>\r\n<!--\r\n\tSpectral by HTML5 UP\r\n\thtml5up.net | @n33co\r\n\tFree for personal and commercial use under the CCA 3.0 license (html5up.net/license)\r\n-->\r\n<html id='site'>\r\n\t<body>\r\n\t\t<script src=\"assets/js/nunjucks.min.js\"></script>\t\t\r\n\t\t<script>\r\n\t\tvar env = new nunjucks.configure('/magda-health', {});\r\n\t\tvar res = nunjucks.render('contact-src.html');\r\n\t\tvar newDoc = document.open(\"text/html\", \"replace\");\r\n\t\tnewDoc.write(res);\r\n\t\tnewDoc.close();\r\n\t\t</script>\r\n\t</body>\r\n</html>";
+cb(null, output);
+;
+} catch (e) {
+  cb(runtime.handleError(e, lineno, colno));
+}
+}
+return {
+root: root
+};
+})();
+})();
+
+(function() {(window.nunjucksPrecompiled = window.nunjucksPrecompiled || {})["index.html"] = (function() {function root(env, context, frame, runtime, cb) {
+var lineno = null;
+var colno = null;
+var output = "";
+try {
+output += "<!DOCTYPE HTML>\r\n<!--\r\n\tSpectral by HTML5 UP\r\n\thtml5up.net | @n33co\r\n\tFree for personal and commercial use under the CCA 3.0 license (html5up.net/license)\r\n-->\r\n<html id='site'>\r\n\t<body>\r\n\t\t<script src=\"assets/js/nunjucks.min.js\"></script>\t\t\r\n\t\t<script>\r\n\t\tvar env = new nunjucks.configure('/magda-health', {});\r\n\t\tvar res = nunjucks.render('index-src.html');\r\n\t\tvar newDoc = document.open(\"text/html\", \"replace\");\r\n\t\tnewDoc.write(res);\r\n\t\tnewDoc.close();\r\n\t\t</script>\r\n\t</body>\r\n</html>";
+cb(null, output);
+;
+} catch (e) {
+  cb(runtime.handleError(e, lineno, colno));
+}
+}
+return {
+root: root
+};
+})();
+})();
+
+(function() {(window.nunjucksPrecompiled = window.nunjucksPrecompiled || {})["meet.html"] = (function() {function root(env, context, frame, runtime, cb) {
+var lineno = null;
+var colno = null;
+var output = "";
+try {
+output += "<!DOCTYPE HTML>\r\n<!--\r\n\tSpectral by HTML5 UP\r\n\thtml5up.net | @n33co\r\n\tFree for personal and commercial use under the CCA 3.0 license (html5up.net/license)\r\n-->\r\n<html id='site'>\r\n\t<body>\r\n\t\t<script src=\"assets/js/nunjucks.min.js\"></script>\t\t\r\n\t\t<script>\r\n\t\tvar env = new nunjucks.configure('/magda-health', {});\r\n\t\tvar res = nunjucks.render('meet-src.html');\r\n\t\tvar newDoc = document.open(\"text/html\", \"replace\");\r\n\t\tnewDoc.write(res);\r\n\t\tnewDoc.close();\r\n\t\t</script>\r\n\t</body>\r\n</html>";
+cb(null, output);
+;
+} catch (e) {
+  cb(runtime.handleError(e, lineno, colno));
+}
+}
+return {
+root: root
+};
+})();
+})();
+
+(function() {(window.nunjucksPrecompiled = window.nunjucksPrecompiled || {})["testimonials.html"] = (function() {function root(env, context, frame, runtime, cb) {
+var lineno = null;
+var colno = null;
+var output = "";
+try {
+output += "<!DOCTYPE HTML>\r\n<!--\r\n\tSpectral by HTML5 UP\r\n\thtml5up.net | @n33co\r\n\tFree for personal and commercial use under the CCA 3.0 license (html5up.net/license)\r\n-->\r\n<html id='site'>\r\n\t<body>\r\n\t\t<script src=\"assets/js/nunjucks.min.js\"></script>\t\t\r\n\t\t<script>\r\n\t\tvar env = new nunjucks.configure('/magda-health', {});\r\n\t\tvar res = nunjucks.render('testimonials-src.html');\r\n\t\tvar newDoc = document.open(\"text/html\", \"replace\");\r\n\t\tnewDoc.write(res);\r\n\t\tnewDoc.close();\r\n\t\t</script>\r\n\t</body>\r\n</html>";
+cb(null, output);
+;
+} catch (e) {
+  cb(runtime.handleError(e, lineno, colno));
+}
+}
+return {
+root: root
+};
+})();
+})();
+
